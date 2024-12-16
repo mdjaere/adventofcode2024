4ceb14a0207e6b16e9a8849d3c7e4880f60a66a9
Martin D. Jaere <mdjaere@gmail.com>
2024-12-16 10:50:26 +0000

    Day12

--- 12/python/solve.py
+++ 12/python/solve.py
@@ -4,64 +4,74 @@ from itertools import product
 infile = sys.argv[1] if len(sys.argv) > 1 else "input"
 data = [list(line.strip()) for line in open(infile)]
 
-R = len(data)
-C = len(data[0])
+RR = range(len(data))
+CR = range(len(data[0]))
+
+one_side = [(1, 1, 0, 0), (0, 1, 0, 1), (0, 0, 1, 1), (1, 0, 1, 0)]
+one_corner = [
+    (1, 0, 0, 0),
+    (0, 1, 0, 0),
+    (0, 0, 1, 0),
+    (0, 0, 0, 1),
+    (0, 1, 1, 1),
+    (1, 0, 1, 1),
+    (1, 1, 0, 1),
+    (1, 1, 1, 0),
+]
+two_corners = [
+    (1, 0, 0, 1),
+    (0, 1, 1, 0),
+]
+no_sides_or_corners = [(0, 0, 0, 0), (1, 1, 1, 1)]
 
 
-def count_sides_and_corners(field, r_min, r_max, c_min, c_max):
-    # March squares
+def count_sides_and_corners(field):
+    # Marchy squary
     corner_count = 0
     side_count = 0
-    for ri, ci in product(range(r_min - 1, r_max + 1), range(c_min - 1, c_max + 1)):
+    rs, cs = zip(*field)
+    for ri, ci in product(
+        range(min(rs) - 1, max(rs) + 1), range(min(cs) - 1, max(cs) + 1)
+    ):
         read_area = tuple(
             (ri + x, ci + y) in field for x, y in [(0, 0), (1, 0), (0, 1), (1, 1)]
         )
-        if sum(read_area) in [0, 4]:
+        if read_area in no_sides_or_corners:
             continue
-        elif sum(read_area) in [1, 3]:
-            corner_count += 1
-        elif read_area in [
-            (1, 0, 0, 1),
-            (0, 1, 1, 0),
-        ]:
-            corner_count += 2
-        elif sum(read_area) == 2:
+        elif read_area in one_side:
             side_count += 1
+        elif read_area in one_corner:
+            corner_count += 1
+        elif read_area in two_corners:
+            corner_count += 2
+        else:
+            assert False
     return side_count, corner_count
 
 
 seen = set()
 sorted_fields = []
 
-for ri, ci in product(range(R), range(C)):
+for ri, ci in product(RR, CR):
     if (ri, ci) in seen:
         continue
     seen.add((ri, ci))
     current_value = data[ri][ci]
     current_field = [(ri, ci)]
-    r_min, r_max, c_min, c_max = ri, ri, ci, ci
     to_search = [(ri, ci)]
     while to_search:
         s_ri, s_ci = to_search.pop()
         for n_ri, n_ci in [
             (s_ri + x, s_ci + y) for x, y in [(-1, 0), (0, 1), (1, 0), (0, -1)]
         ]:
-            if n_ri not in range(R) or n_ci not in range(C):
+            if n_ri not in RR or n_ci not in CR:
                 continue
             if data[n_ri][n_ci] != current_value or (n_ri, n_ci) in seen:
                 continue
             current_field.append((n_ri, n_ci))
-            r_min, r_max, c_min, c_max = (
-                min(r_min, n_ri),
-                max(r_max, n_ri),
-                min(c_min, n_ci),
-                max(c_max, n_ci),
-            )
             seen.add((n_ri, n_ci))
             to_search.append((n_ri, n_ci))
-    side_count, corner_count = count_sides_and_corners(
-        current_field, r_min, r_max, c_min, c_max
-    )
+    side_count, corner_count = count_sides_and_corners(current_field)
     sorted_fields.append((len(current_field), side_count, corner_count))
 
 part_1 = sum(f * (s + c) for f, s, c in sorted_fields)
